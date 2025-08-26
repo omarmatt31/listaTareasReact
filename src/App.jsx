4a3c01ddc0588e1483c2c4300c0bd14f4dc393c7
Omar Mattos <omarmatt84@gmail.com>
2025-08-26 19:02:39 -0300

    se creo la estructura del proyecto y se instaló algunas dependencias

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <h1>Hola Mundo</h1>
+      </main>
+      <footer className="bg-dark text-light text-center py-3">
+        <p>&copy; Todos los derechos reservados</p>
+        <p>Omar Mattos 💻</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
