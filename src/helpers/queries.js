bf1e231ee878f18895899ef4c10a48065f260f93
Omar Mattos <omarmatt84@gmail.com>
2025-08-26 20:54:08 -0300

    agregue el archivo de queries

---
+++ src/helpers/queries.js
@@ -0,0 +1,50 @@
+const urltareas = import.meta.env.VITE_API_TAREAS
+
+
+export const leerTareas = async()=>{
+    try{
+        const respuesta = await fetch(urltareas)
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+}
+
+export const obtenerTareaPorId = async(id)=>{
+    try{
+        const respuesta = await fetch(urltareas+`/${id}`)
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+}
+
+export const crearProducto = async(tareaNueva)=>{
+    try{
+        const respuesta = await fetch(urltareas, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(tareaNueva)
+        })
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+}
+
+export const borrarTareaPorId = async(id)=>{
+    try{
+        const respuesta = await fetch(urltareas+`/${id}`, {
+            method: 'DELETE',
+        })
+        return respuesta
+    }catch(error){
+        console.error(error);
+        return null
+    }
+}
